a6b21349161db4fc7f3336e1d9766f66aaae2da3
Uian Sol <sol.uian@gmail.com>
2020-03-24 13:40:11 -0300

    Add endpoint for login

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,8 +2,11 @@ const express = require('express')
 const OngController = require('./controllers/OngController')
 const ProfileController = require('./controllers/ProfileController')
 const IncidentController = require('./controllers/IncidentController')
+const SessionController = require('./controllers/SessionController')
 
 const routes = express.Router()
+
+routes.post('/sessions', SessionController.create)
 
 routes.get('/ongs', OngController.index)
 routes.post('/ongs', OngController.create)
