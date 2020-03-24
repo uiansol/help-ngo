08a143c0844664acea1347d3271efdf788074d49
Uian Sol <sol.uian@gmail.com>
2020-03-24 10:44:40 -0300

    Create migrations for ongs and incidents

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -5,8 +5,12 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
-    }
+      filename: './src/database/dev.sqlite'
+    },
+    migrations: {
+      directory: './src/database/migrations'
+    },
+    useNullAsDefault: true
   },
 
   staging: {
@@ -41,4 +45,4 @@ module.exports = {
     }
   }
 
-};
+}
