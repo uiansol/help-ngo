5959c27ab7b4023cbd78066f7838655511198911
Uian Sol <sol.uian@gmail.com>
2020-03-25 10:42:03 -0300

    Add router to manage application

---
+++ frontend/src/routes.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import Logon from './pages/Logon'
+
+function Routes () {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' component={Logon} />
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+export default Routes
