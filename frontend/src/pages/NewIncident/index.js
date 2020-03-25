fd6b28d7ac7a126fb4c7c3bef66176cbe07ed2b6
Uian Sol <sol.uian@gmail.com>
2020-03-25 14:28:12 -0300

    Connect new incident with backend

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,12 +1,44 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
+
+import api from '../../services/api'
 
 import './styles.css'
 
 import logoImg from '../../assets/logo.svg'
 
 function NewIncident () {
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+  const [value, setValue] = useState('')
+
+  const history = useHistory()
+
+  const ongId = localStorage.getItem('ongId')
+
+  async function handleNewIncident (evt) {
+    evt.preventDefault()
+
+    const data = {
+      title,
+      description,
+      value
+    }
+
+    try {
+      await api.post('incidents', data, {
+        headers: {
+          Authorization: ongId
+        }
+      })
+
+      history.push('/profile')
+    } catch (error) {
+      alert('Erro on case register, try ir again!')
+    }
+  }
+
   return (
     <div className='new-incident-container'>
       <div className='content'>
@@ -22,11 +54,23 @@ function NewIncident () {
           </Link>
         </section>
 
-        <form>
-          <input placeholder='Case title' />
-          <textarea placeholder='Description' />
+        <form onSubmit={handleNewIncident}>
+          <input
+            placeholder='Case title'
+            value={title}
+            onChange={evt => setTitle(evt.target.value)}
+          />
+          <textarea
+            placeholder='Description'
+            value={description}
+            onChange={evt => setDescription(evt.target.value)}
+          />
 
-          <input placeholder='Value in dollars' />
+          <input
+            placeholder='Value in dollars'
+            value={value}
+            onChange={evt => setValue(evt.target.value)}
+          />
 
           <button className='button' type='submit'>Register</button>
         </form>
