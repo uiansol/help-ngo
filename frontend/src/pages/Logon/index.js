e42ea945137c5d480e582d552cc0fcf584d24a1f
Uian Sol <sol.uian@gmail.com>
2020-03-25 10:28:36 -0300

    Create login page

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import { FiLogIn } from 'react-icons/fi'
+
+import './styles.css'
+
+import logoImg from '../../assets/logo.svg'
+import heroesImg from '../../assets/heroes.png'
 
 function Logon () {
   return (
-    <h1>Hello world</h1>
+    <div className='logon-container'>
+      <section className='form'>
+        <img src={logoImg} alt='Be The Hero' />
+
+        <form>
+          <h1>Login</h1>
+
+          <input placeholder='Your ID' />
+          <button className='button' type='submit'>Enter</button>
+
+          <a href='/register'>
+            <FiLogIn size={16} color='#E02041' />
+            I don't have an account
+          </a>
+        </form>
+      </section>
+
+      <img src={heroesImg} alt='Heroes' />
+    </div>
   )
 }
 
