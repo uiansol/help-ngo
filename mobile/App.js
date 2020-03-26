ec051bed23de14e631b292f51ecfe5b2e93a8fed
Uian Sol <sol.uian@gmail.com>
2020-03-26 14:29:10 -0300

    Add blank pages and routing

--- mobile/App.js
+++ mobile/App.js
@@ -1,11 +1,9 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
 
-export default function App() {
+import Routes from './src/routes'
+
+export default function App () {
   return (
-    <View>
-      <Text>Hello world!</Text>
-    </View>
-  );
+    <Routes />
+  )
 }
-
